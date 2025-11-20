35041afd420086cf8a9df88baa4c566046f6ca2f
kazuki404313 <kazuki20050404@gmail.com>
2025-11-20 14:44:36 +0900

    ファイル追加
    kazuki

---
+++ Sagyo2.py
@@ -0,0 +1,19 @@
+import mlx_whisper
+from pydub import AudioSegment
+import numpy as np
+
+class Sagyo2:
+    """音声ファイルを指定して文字起こしをするクラス。
+    Returns:
+        result: 文字起こしの結果を含む関数。
+    """
+
+    def transcribe_audio(self):
+
+    # 音声ファイルを指定して文字起こし
+        audio_file_path  = 'audio.wav'
+
+        result = mlx_whisper.transcribe(audio_file_path, path_or_hf_repo="./whisper-base-mlx")
+        print(result["text"])
+
+        return result["text"]
